1f07dbde1891db1d1a1f466c6f7166c7860347e7
wbe <34304532+wbecerro@users.noreply.github.com>
2025-05-26 19:11:55 +0200

    Cambiadas dependencias.
    Ahora si no se tiene una de las dependencias simplemente las funciones con ese plugin se desactivan.

--- src/main/java/wbe/laboursOfHercules/listeners/EventListeners.java
+++ src/main/java/wbe/laboursOfHercules/listeners/EventListeners.java
@@ -1,5 +1,6 @@
 package wbe.laboursOfHercules.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import wbe.laboursOfHercules.LaboursOfHercules;
 
@@ -12,15 +13,25 @@ public class EventListeners {
 
         pluginManager.registerEvents(new BlockBreakListeners(), plugin);
         pluginManager.registerEvents(new CraftItemListeners(), plugin);
-        pluginManager.registerEvents(new CrateOpenListeners(), plugin);
         pluginManager.registerEvents(new EnchantItemListeners(), plugin);
         pluginManager.registerEvents(new EntityDeathListeners(), plugin);
         pluginManager.registerEvents(new EntityTameListeners(), plugin);
-        pluginManager.registerEvents(new MythicMobDeathListeners(), plugin);
         pluginManager.registerEvents(new PlayerFishListeners(), plugin);
-        pluginManager.registerEvents(new PlayerReceiveRewardListeners(), plugin);
         pluginManager.registerEvents(new PlayerShearEntityListeners(), plugin);
         pluginManager.registerEvents(new InventoryClickListeners(), plugin);
         pluginManager.registerEvents(new PlayerInteractListeners(), plugin);
+
+        if(Bukkit.getPluginManager().getPlugin("CrazyCrates") != null) {
+            pluginManager.registerEvents(new CrateOpenListeners(), plugin);
+        }
+
+        if(Bukkit.getPluginManager().getPlugin("MythicMobs") != null) {
+            pluginManager.registerEvents(new MythicMobDeathListeners(), plugin);
+        }
+
+        if(Bukkit.getPluginManager().getPlugin("YggdrasilsBark") != null
+                && Bukkit.getPluginManager().getPlugin("AquaticLostWealth") != null) {
+            pluginManager.registerEvents(new PlayerReceiveRewardListeners(), plugin);
+        }
     }
 }
